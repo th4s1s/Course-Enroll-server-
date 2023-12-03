1919d143d9a64059a343b9f0cd705217c56e6598
Lio <bentnt1982@gmail.com>
2023-12-03 15:12:51 +0700

    done class route

---
+++ server/controller/classController.js
@@ -0,0 +1,79 @@
+const pool = require('../db').pool
+
+async function getAllClasses(req, res) {
+    try {
+        SemID = req.params.semester
+        await pool.query('SELECT * FROM class WHERE SemID = $1 ORDER BY CourseID, ClassID', [SemID], (error, results) => {
+            if (error) {
+                return res.status(500).json({
+                    msg: error.message,
+                })
+            }
+            return res.status(200).json({
+                class: results.rows
+            })
+        })
+    } catch(error) {
+        return res.status(500).json({
+            msg: error.message,
+        })
+    }
+}
+
+async function enrollClass(req, res) {
+    if(!req.body.susername || !req.body.class || !req.body.semester || !req.body.course) {
+        return res.status(400).json({
+            msg: 'Bad request'
+        })
+    }
+    try {
+        student = req.body.susername
+        classID = req.body.class
+        SemID = req.body.semester
+        CourseID = req.body.course
+        await pool.query('CALL enroll($1, $2, $3, $4)', [classID, CourseID, SemID, student], (error, results) => {
+            if (error) {
+                return res.status(500).json({
+                    msg: error.message,
+                })
+            }
+            return res.status(200).json({
+                msg: 'Đăng ký thành công',
+            })
+        })
+    } catch(error) {
+        return res.status(500).json({
+            msg: error.message,
+        })
+    }
+}
+
+async function unenrollClass(req, res) {
+    if(!req.body.susername || !req.body.class || !req.body.semester || !req.body.course) {
+        return res.status(400).json({
+            msg: 'Bad request'
+        })
+    }
+    try {
+        student = req.body.susername
+        classID = req.body.class
+        SemID = req.body.semester
+        CourseID = req.body.course
+        await pool.query('CALL unenroll($1, $2, $3, $4)', [classID, CourseID, SemID, student], (error, results) => {
+            if (error) {
+                return res.status(500).json({
+                    msg: error.message,
+                })
+            }
+            return res.status(200).json({
+                msg: 'Hủy đăng ký thành công',
+            })
+        })
+    } catch(error) {
+        return res.status(500).json({
+            msg: error.message,
+        })
+    }
+}
+
+module.exports = {getAllClasses , enrollClass, unenrollClass}
---
+++ server/routes/studentRoute.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+
+// controller
+const studentCtl = require('../controller/studentController');
